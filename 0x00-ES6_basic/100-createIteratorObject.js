1264c7325a5c84aece11dd49ea3732a39bab87ff
girma <girmatarekegn.gi@gmail.com>
2022-12-25 21:03:44 +0300

    Add last two ass

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,10 +1,10 @@
 export default function createIteratorObject(report) {
   const emps = [];
-   // eslint-disable-next-line
-  for (const dep of Object.keys(report.allEmployees)) {
   // eslint-disable-next-line
-  for (const emp of report.allEmployees[dep]) {
-    emps.push(emp);
+  for (const dep of Object.keys(report.allEmployees)) {
+    // eslint-disable-next-line
+    for (const emp of report.allEmployees[dep]) {
+      emps.push(emp);
     }
   }
   return emps;
