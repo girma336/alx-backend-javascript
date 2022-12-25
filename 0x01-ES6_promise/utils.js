599f6bb0a13b0a4eb27c24f5e0f1722f8407b47b
girma <girmatarekegn.gi@gmail.com>
2022-12-25 22:26:29 +0300

    Add after chenge node version

--- 0x01-ES6_promise/utils.js
+++ 0x01-ES6_promise/utils.js
@@ -1,9 +1,14 @@
-const uploadPhoto = {
-  status: 200,
-  body: 'photo-profile-1',
-};
-
- const createUser = {
-  firstName: 'Guillaume',
-  lastName: 'Salva',
-}
+export function uploadPhoto() {
+    return Promise.resolve({
+      status: 200,
+      body: 'photo-profile-1',
+    });
+  }
+  
+  
+  export function createUser() {
+    return Promise.resolve({
+      firstName: 'Guillaume',
+      lastName: 'Salva',
+    });
+  }
